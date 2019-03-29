b2a9991e205e43db27736d132ef145497ff449cd
Thang Le <tug18152@temple.edu>
2019-03-29 01:40:05 -0400

    deep learning

--- fineTreePred.m
+++ fineTreePred.m
@@ -1,3 +1,6 @@
+%shuffling data, has to do in command window
+% random_EMG = EMGrawdataS5(randperm(size(EMGrawdataS5,1), :)); 
 
 yfit = fineTree.predictFcn(random_EMG)
 
+%incorporate output with C code
