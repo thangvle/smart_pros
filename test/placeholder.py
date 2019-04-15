aa4f2cbb8fefe0e8c3a5acace0da3c8b45866334
Thang Le <tug18152@temple.edu>
2019-04-15 18:10:29 -0400

    change matrix syntax

--- test/placeholder.py
+++ test/placeholder.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 a = tf.placeholder(tf.float32, shape =[3])
-b = tf.constant([5 5 5], tf.float32)
+b = tf.constant([5, 5, 5], tf.float32)
 
 # a is used as constant rather than placeholder
 c = a + b
@@ -9,6 +9,5 @@ c = a + b
 writer = tf.summary.FileWriter('graph/placeholders', tf.get_default_graph())
 with tf.Session() as sess:
     # compute c as a + b (add in matrix)
-    print(sess.run(c, {a: [1 2 3]}))    # result c = [6 7 8]
+    print(sess.run(c, {a: [1, 2, 3]}))    # result c = [6 7 8]
     writer.close()
-    
