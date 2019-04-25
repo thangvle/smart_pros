c93f7306833eda88c4c6558bd6721732e73c11c9
Thang Le <tug18152@temple.edu>
2019-04-25 10:09:19 -0400

    xupdated

--- matlab_training/fineTreePred.m
+++ matlab_training/fineTreePred.m
@@ -1,2 +1,23 @@
-options = trainingOptions('sgdm');
-network = trainNetwork(EMGrawdataS1, layers_1, options)
+%a = arduino; % initialize arduino
+%pin = 11; % change the pin here
+
+inputData = [];
+
+for i=1:299
+    inputData(i) = i; % input data here
+end
+predictData = transpose(inputData)
+
+%while(1)
+%   yfit = fineTree.predictFcn(predictData)
+%   
+%end
+
+% TODO:
+% yfit return an array of prediction. Match each of these predict
+% accordingly 
+% Option 1: try queue
+% Option 2: try hash map. Hash map returns a value but there will be a lot
+% of duplication
+
+
